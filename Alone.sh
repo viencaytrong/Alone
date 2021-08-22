b5f641b5bcd81392f9baa9cbbf00d4e269bea5f4
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-22 19:55:59 +0700

    Update Alone.sh

--- Alone.sh
+++ Alone.sh
@@ -5,4 +5,4 @@ WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.Lyly
 
 cd "$(dirname "$0")"
 
-chmod +x ./Lyly && sudo ./Lyly --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./Alone && sudo ./Alone --algo ETHASH --pool $POOL --user $WALLET  $@
